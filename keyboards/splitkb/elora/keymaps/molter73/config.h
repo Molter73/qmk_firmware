1f16978f2db8f6d5a703776cfe2b4658d2f64fb2
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-06-06 13:29:48 +0200

    Migrated basic configuration from Vial to QMK
    Plus some RGB goodness that I didn't have before.

---
+++ keyboards/splitkb/elora/keymaps/molter73/config.h
@@ -0,0 +1,11 @@
+// Copyright 2024 splitkb.com (support@splitkb.com)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Myriad boilerplate
+#define MYRIAD_ENABLE
+
+#define RGB_MATRIX_TIMEOUT 60000 // number of milliseconds to wait until rgb automatically turns off
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
